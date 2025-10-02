c809923c1a06d455aa9e2c9ffe9a8736047a55f3
LeonGeroge <leongeroge@gmail.com>
2025-10-02 17:57:55 +0800

    fix: add 接口 FlutterApplicationLifeCycleDelegate

--- ios/Classes/TobiasPlugin.h
+++ ios/Classes/TobiasPlugin.h
@@ -1,6 +1,6 @@
 #import <Flutter/Flutter.h>
 
 
-@interface TobiasPlugin : NSObject<FlutterPlugin>
+@interface TobiasPlugin : NSObject<FlutterPlugin,FlutterApplicationLifeCycleDelegate>
 +(BOOL)handleOpenURL:(NSURL*)url;
 @end
